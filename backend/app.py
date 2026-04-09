6e07b1038c1ebf8ab3a59612d3d65bb5c00e9c6e
TaylorBundy <martonbarbosa@hotmail.com>
2026-04-09 19:40:53 -0300

    first

---
+++ backend/app.py
@@ -0,0 +1,114 @@
+from flask import Flask, request, jsonify
+import requests
+import os
+from git import Repo
+# from flask import Flask, request, jsonify
+from flask_cors import CORS  # 👈 IMPORTANTE
+
+app = Flask(__name__)
+CORS(app)  # 👈 ESTO SOLUCIONA TODO
+
+# app = Flask(__name__)
+
+GITHUB_API = "https://api.github.com"
+GITHUB_TOKEN = "token"
+
+@app.route("/repos")
+def get_repos():
+    token = request.headers.get("Authorization")
+    
+    #headers = {
+        #"Authorization": token
+    #}
+    headers = {
+        "Authorization": f"Bearer {GITHUB_TOKEN}"
+    }
+
+    r = requests.get(f"{GITHUB_API}/user/repos", headers=headers)
+    # print("STATUS:", r.status_code)
+    # print("RESPONSE:", r.text)
+    return jsonify(r.json())
+
+
+@app.route("/clone", methods=["POST"])
+def clone_repo():
+    data = request.json
+    url = data["url"]
+    name = data["name"]
+
+    path = f"repos/{name}"
+
+    if not os.path.exists(path):
+        Repo.clone_from(url, path)
+
+    return jsonify({"status": "clonado"})
+
+def construir_arbol(directorio):
+    arbol = []
+
+    for item in os.listdir(directorio):
+        ruta = os.path.join(directorio, item)
+
+        if os.path.isdir(ruta):
+            arbol.append({
+                "name": item,
+                "type": "folder",
+                "children": construir_arbol(ruta)
+            })
+        else:
+            arbol.append({
+                "name": item,
+                "type": "file",
+                "path": ruta.replace("\\", "/")
+            })
+
+    return arbol
+
+
+@app.route("/tree")
+def get_tree():
+    repo = request.args.get("repo")
+    path = f"repos/{repo}"
+
+    arbol = construir_arbol(path)
+    return jsonify(arbol)
+
+
+@app.route("/files")
+def list_files():
+    repo = request.args.get("repo")
+    path = f"repos/{repo}"
+
+    files = []
+    for root, dirs, filenames in os.walk(path):
+        for f in filenames:
+            files.append(os.path.join(root, f))
+
+    return jsonify(files)
+
+
+@app.route("/file")
+def read_file():
+    path = request.args.get("path")
+
+    with open(path, "r", encoding="utf-8") as f:
+        return f.read()
+
+
+@app.route("/save", methods=["POST"])
+def save_file():
+    data = request.json
+    path = data["path"]
+    content = data["content"]
+
+    with open(path, "w", encoding="utf-8") as f:
+        f.write(content)
+
+    repo = Repo(os.path.dirname(path))
+    repo.git.add(A=True)
+    repo.index.commit("Update desde web")
+
+    return jsonify({"status": "guardado"})
+
+
+app.run(debug=True)
---
+++ frontend/app.js
@@ -0,0 +1,128 @@
+let token = "";
+let archivoActual = "";
+const nombreArchivo = document.querySelector("#NombreArchivo");
+
+async function cargarRepos() {
+  // token = document.getElementById("token").value;
+  const res = await fetch("http://localhost:5000/repos");
+
+  // const res = await fetch("http://localhost:5000/repos", {
+  //   headers: {
+  //     // Authorization: "token " + token,
+  //     Authorization: "Bearer " + token,
+  //   },
+  // });
+
+  const repos = await res.json();
+
+  const lista = document.getElementById("repos");
+  lista.innerHTML = "";
+
+  repos.forEach((r) => {
+    const li = document.createElement("li");
+    li.textContent = r.name;
+    //li.onclick = () => clonarRepo(r);
+    li.onclick = async () => {
+      cargarRepos();
+      await clonarRepo(r);
+      cargarArbol(r.name);
+    };
+    lista.appendChild(li);
+  });
+}
+
+async function clonarRepo(repo) {
+  await fetch("http://localhost:5000/clone", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      url: repo.clone_url,
+      name: repo.name,
+    }),
+  });
+
+  cargarArchivos(repo.name);
+}
+
+async function cargarArchivos(repo) {
+  const res = await fetch(`http://localhost:5000/files?repo=${repo}`);
+  const files = await res.json();
+
+  archivoActual = files[0];
+  const elNombre = archivoActual.split("\\")[1];
+
+  const contenido = await fetch(
+    `http://localhost:5000/file?path=${archivoActual}`,
+  );
+  document.getElementById("editor").value = await contenido.text();
+  nombreArchivo.textContent = elNombre;
+}
+
+async function guardar() {
+  const contenido = document.getElementById("editor").value;
+
+  await fetch("http://localhost:5000/save", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      path: archivoActual,
+      content: contenido,
+    }),
+  });
+
+  alert("Guardado");
+}
+
+async function cargarArbol(repo) {
+  const res = await fetch(`http://localhost:5000/tree?repo=${repo}`);
+  const data = await res.json();
+
+  const tree = document.getElementById("tree");
+  tree.innerHTML = "";
+
+  renderTree(data, tree);
+}
+
+function renderTree(nodes, container) {
+  nodes.forEach((node) => {
+    const div = document.createElement("div");
+    div.classList.add("tree-item");
+    //console.log(node.name);
+
+    if (node.type === "folder") {
+      div.textContent = "📁 " + node.name;
+      div.classList.add("folder");
+
+      const childrenContainer = document.createElement("div");
+      childrenContainer.style.display = "none";
+      childrenContainer.style.marginLeft = "15px";
+
+      div.onclick = () => {
+        childrenContainer.style.display =
+          childrenContainer.style.display === "none" ? "block" : "none";
+      };
+
+      container.appendChild(div);
+      container.appendChild(childrenContainer);
+
+      renderTree(node.children, childrenContainer);
+    }
+
+    if (node.type === "file") {
+      div.textContent = "📄 " + node.name;
+      div.classList.add("file");
+
+      div.onclick = async () => {
+        const res = await fetch(`http://localhost:5000/file?path=${node.path}`);
+
+        const contenido = await res.text();
+
+        archivoActual = node.path;
+        document.getElementById("editor").value = contenido;
+        nombreArchivo.textContent = node.name;
+      };
+
+      container.appendChild(div);
+    }
+  });
+}
